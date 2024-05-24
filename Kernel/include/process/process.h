1ec9b8a5b5878facccf5ca0e00c671a6022b82dd
MatiSapino <msapino@itba.edu.ar>
2024-05-24 16:07:27 -0300

    process and process context created

--- Kernel/include/process/process.h
+++ Kernel/include/process/process.h
@@ -1,9 +1,79 @@
 #ifndef _PROCESS_H_
 #define _PROCESS_H_
 
+#include <memory_manager.h>
+#include <defs.h>
+
 #define P_INIT_EFLAGS 0x202
 #define P_INIT_CS 0x8
 #define P_SS 0
 #define P_EXIT_CODE 0
+
+#define LOWEST 1
+#define MEDIUM 5
+#define HIGHEST 10
+
+#define K_PROCESS_STACK_SIZE (1024 * 4)
+
+typedef enum pstatus
+{
+    WAITING = 0,
+    READY,
+    TERMINATED,
+} pstatus_t;
+
+typedef int pid_t;
+
+typedef struct context
+{
+    uint64_t r15;
+    uint64_t r14;
+    uint64_t r13;
+    uint64_t r12;
+    uint64_t r11;
+    uint64_t r10;
+    uint64_t r9;
+    uint64_t r8;
+
+    uint64_t rsi;
+    uint64_t rdi;
+    uint64_t rbp;
+
+    uint64_t rdx;
+    uint64_t rcx;
+    uint64_t rbx;
+    uint64_t rax;
+
+    uint64_t rip;
+    uint64_t cs;
+    uint64_t eflags;
+    uint64_t rsp;
+    uint64_t ss;
+} context_t;
+
+typedef struct process process_t;
+
+typedef struct process
+{
+    uint8_t k_stack[K_PROCESS_STACK_SIZE];
+    context_t *context;
+    pid_t pid;
+    pstatus_t status;
+    process_t *parent;
+    dataDescriptor_t dataDescriptors[128];
+    size_t dataD_index;
+    void *channel;
+    list_ptr children;
+    int exit_status;
+    int priority;
+    char **argv;
+    int argc;
+} process_t;
+
+typedef int (*function_t)(int, char *[]);
+
+process_t *new_process(function_t main, int argc, char *argv[]);
+
+void free_process(process_t *process);
 
 #endif
--- Kernel/process/process.c
+++ Kernel/process/process.c
@@ -1,3 +1,99 @@
 #include <process.h>
 
-// static int last_pid = 0;
+static int last_pid = 0;
+
+static void start(function_t function, int argc, char *argv[])
+{
+    int status = function(argc, argv);
+    sys_exit(status);
+}
+
+static int search_by_pid(void *process, void *pid)
+{
+    return ((process_t *)process)->pid == pid;
+}
+
+static char **get_argv_copy(int argc, char *argv[])
+{
+    if (argc == 0 || argv == NULL)
+        return NULL;
+
+    char **argv_copy = malloc(argc * sizeof(char *));
+    for (size_t i = 0; i < argc; i++)
+    {
+        argv_copy[i] = malloc(strlen(argv[i]) + 1);
+        strcpy(argv_copy[i], argv[i]);
+    }
+
+    return argv_copy;
+}
+
+static context_t *get_init_context(process_t *process, function_t main,
+                                   int argc, char *argv[])
+{
+    context_t *context =
+        (context_t *)((uint64_t)process + K_PROCESS_STACK_SIZE -
+                      sizeof(context_t));
+
+    context->rdi = (uint64_t)main;
+    context->rsi = (uint64_t)argc;
+    context->rdx = (uint64_t)argv;
+    context->rip = (uint64_t)&start;
+
+    context->cs = P_INIT_CS;
+    context->eflags = P_INIT_EFLAGS;
+    context->ss = P_SS;
+
+    context->rsp = (uint64_t)context;
+
+    return context;
+}
+
+process_t *new_process(function_t main, int argc, char *argv[])
+{
+    process_t *process = (process_t *)malloc(sizeof(process_t));
+    if (process == NULL)
+        return NULL;
+
+    process->argv = get_argv_copy(argc, argv);
+    process->argc = argc;
+
+    process->pid = last_pid++;
+    process->priority = LOWEST;
+
+    process->status = READY;
+    process->exit_status = -1;
+
+    process->channel = NULL;
+    process->parent = NULL;
+    process->children = new_linked_list((int (*)(void *, void *))search_by_pid);
+
+    process->context =
+        get_init_context(process, main, process->argc, process->argv);
+
+    /* Creates stdin in dataDescriptor 0*/
+    process->dataDescriptors[0] = create_dataDescriptor(STD_T, READ_MODE);
+
+    /* Creates stdout in dataDescriptor 0*/
+    process->dataDescriptors[1] = create_dataDescriptor(STD_T, WRITE_MODE);
+
+    process->dataD_index = 2;
+
+    return process;
+}
+
+void free_process(process_t *process)
+{
+    free_list(process->children);
+
+    // free arguments
+    if (process->argc)
+    {
+        for (size_t i = 0; i < process->argc; i++)
+            kfree(process->argv[i]);
+
+        kfree(process->argv);
+    }
+
+    kfree(process);
+}

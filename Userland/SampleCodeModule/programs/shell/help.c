038e479d5a5cb01e1d276e975593ecc6fb1a6c11
sant9s <sgalarraga@itba.edu.ar>
2024-06-09 17:30:10 -0300

    added a bunch of help descriptions

--- Userland/SampleCodeModule/programs/shell/help.c
+++ Userland/SampleCodeModule/programs/shell/help.c
@@ -9,7 +9,7 @@ static void formatter(char *name1, char *name2, char *name3, char *name4) {
 static void help_cmds() {
     own_printf("\nAvilable commands:\n");
     formatter("clear", "time", "fibonacci", "primes");
-    formatter("processstatus", "mem", "sem", "kill");
+    formatter("ps", "mem", "sem", "kill");
     formatter("nice", "block", "cat", "wc");
     formatter("filter", "pipe", "phylo", "loop");
     formatter("printmem", "divzero", "invopcode", "welcome");
@@ -114,25 +114,86 @@ static void help_pipe() {
     puts("");
 }
 
+static void help_ps(){
+    own_printf("Command:\n   ps         Lists running processes with their pids\n");
+}
+
+static void help_mem(){
+    own_printf("Command:\n mem          Shows memory usage status in bytes\n");
+}
+
+static void help_sem(){
+    own_printf("Command:\n sem          Shows all active semaphores\n");
+}
+
+static void help_kill(){
+    own_printf("Command:\n kill [pid]   Recieves a pid and kills that process\n");  
+}
+
+static void help_nice(){
+    own_printf("Command:\n nice [pid] [prio]  Recieves a pid and modifies the priority of the process\n");  
+}
+
+static void help_block(){
+    own_printf("Command:\n block [pid]        Recieves a pid and changes the status to waiting\n");  
+}
+
+static void help_cat(){
+    own_printf("Command:\n cat [???]          Prints through stdout the file recieved\n");                       // falta revisar
+}
+
+static void help_wc(){
+    own_printf("Command:\n wc [???]          Counts lines from input\n");                       // falta revisar
+}
+static void help_filter(){
+    own_printf("Command:\n wc [???]          Filters vowels from input\n");                       // falta revisar
+}
+static void help_phylo(){
+     own_printf("Command:\n phylo            Runs dining philosophers problem simulation\n");                       
+}
+
+static void help_loop(){
+    own_printf("Command:\n loop [seconds]    Prints \"Hello World!\" in a loop for the amount of seconds written          \n");
+}
+
 int help(int argc, char *argv[]) {
     if (argc == 1)
         help_cmds();
 
     else if (strcmp("clear", argv[1]) == 0)
         help_clear();
-
     else if (strcmp("time", argv[1]) == 0)
         help_time();
-
     else if (strcmp("fibonacci", argv[1]) == 0)
         help_fibonacci();
-
     else if (strcmp("primes", argv[1]) == 0)
         help_primes();
-
+    else if(strcmp("ps", argv[1]) == 0)
+        help_ps();
+    else if(strcmp("mem", argv[1]) == 0)
+        help_mem();
     else if (strcmp("printmem", argv[1]) == 0)
         help_printmem();
-
+    else if(strcmp("sem", argv[1]) == 0)
+        help_sem();
+    else if(strcmp("kill", argv[1]) == 0)
+        help_kill();
+    else if(strcmp("nice", argv[1]) == 0)
+        help_nice();
+    else if(strcmp("block", argv[1]) == 0)
+        help_block();
+    else if(strcmp("cat", argv[1]) == 0)
+        help_cat();
+    else if(strcmp("wc", argv[1]) == 0)
+        help_wc();
+    else if(strcmp("filter", argv[1]) == 0)
+        help_filter();
+    else if (strcmp("pipe", argv[1]) == 0)
+        help_pipe();
+    else if (strcmp("phylo", argv[1]) == 0)
+        help_phylo();
+    else if (strcmp("loop", argv[1]) == 0)
+        help_loop();
     else if (strcmp("inforeg", argv[1]) == 0)
         help_inforeg();
 
@@ -148,8 +209,7 @@ int help(int argc, char *argv[]) {
     else if (strcmp("welcome", argv[1]) == 0)
         help_welcome();
 
-    else if (strcmp("pipe", argv[1]) == 0)
-        help_pipe();
+    
 
     else
         help_error(argv[1]);
